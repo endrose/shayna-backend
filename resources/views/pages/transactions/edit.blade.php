b55a7f3d0e6905e5bbd7da2f22dff4b8a029ea3e
Endros <endros007@gmail.com>
2020-03-02 18:52:59 +0700

    first commit

---
+++ resources/views/pages/transactions/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.default')
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <strong>Ubah Transaksi</strong>
+            <small>{{ $item->uuid }}</small>
+        </div>
+        <div class="card-body card-block">
+            <form action="{{ route('transactions.update', $item->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="name" class="form-control-label">Nama Pemesan</label>
+                    <input type="text"
+                           name="name"
+                           value="{{ old('name') ? old('name'): $item->name }}"
+                           class="form-control @error('name') is-invalid @enderror">
+                           @error('name') <div class="text-muted pl-3">{{ $message }}</div> @enderror
+                </div>
+                <div class="form-group">
+                    <label for="email" class="form-control-label">Nama Pemesan</label>
+                    <input type="text"
+                           name="email"
+                           value="{{ old('email') ? old('email'): $item->email }}"
+                           class="form-control @error('email') is-invalid @enderror">
+                           @error('email') <div class="text-muted pl-3">{{ $message }}</div> @enderror
+                </div>
+                <div class="form-group">
+                    <label for="number" class="form-control-label">Nomor HP</label>
+                    <input type="text"
+                           name="number"
+                           value="{{ old('number') ? old('number') : $item->number }}"
+                           class="form-control @error('number') is-invalid @enderror">
+                           @error('number') <div class="text-muted pl-3">{{ $message }}</div> @enderror
+                </div>
+                <div class="form-group">
+                    <label for="address" class="form-control-label ">Alamat Pemesan</label>
+                    <textarea name="address" class="ckeditor form-control @error('address') is-invalid @enderror">
+                        {{ old('address') ? old('address') : $item->address }}
+                    </textarea>
+                    @error('address') <div class="text-muted pl-3">{{ $message }}</div> @enderror
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">
+                        Ubah Transaksi
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
